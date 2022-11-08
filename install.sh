51a6988cb53237655d4a07debf5834d98d438c5a
pricedown <josephmisaacs@gmail.com>
2022-11-07 19:45:29 -0500

    trying to fix install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env sh
 cd
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+yes | ~/.emacs.d/bin/doom install
 
-cp -r xmonad.hs ~/.config/xmonad/xmonad.hs
-cp -r xmobarrc ~/.config/xmobar/xmobarrc
-cp -r init.el ~/.doom.d/init.el
-cp -r config.el ~/.doom.d/config.el
-cp -r config.rasi ~/.config/rofi/config.rasi
-cp -r gruv.rasi ~/.config/rofi/gruv.rasi
-cp -r alacritty.yml ~/.config/alacritty/alacritty.yml
+cd ~/sys-setup
+
+cp -u init.el ~/.doom.d/
+cp -u config.el ~/.doom.d/
+mkdir ~/.config/xmonad
+cp -u xmonad.hs ~/.config/xmonad/
+mkdir ~/.config/xmobar
+cp -u xmobarrc ~/.config/xmobar/
+mkdir ~/.config/rofi
+cp -u config.rasi ~/.config/rofi/
+cp -u gruv.rasi ~/.config/rofi/
+mkdir ~/.config/alacritty
+cp -u alacritty.yml ~/.config/alacritty/
