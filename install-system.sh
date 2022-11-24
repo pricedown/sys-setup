aa8d0a07cf299ff5dd2fd57f0d44d15e103009b8
pricedown <josephmisaacs@gmail.com>
2022-11-23 19:39:02 -0500

    Combining the installer

--- install-system.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
-sudo cp -u ~/sys-setup/system/configuration.nix /etc/nixos/
-sudo cp -u ~/sys-setup/system/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+sudo nix-env -i neovim emacs
+
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+sudo nix-channel --update
+sudo cp -u ~/sys-setup/system/configuration.nix /etc/nixos/
+sudo mkdir /etc/X11
+sudo mkdir /etc/X11/xorg.conf.d
+sudo cp -u ~/sys-setup/system/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
+
 cd ~
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
 yes | ~/.emacs.d/bin/doom install
@@ -24,3 +33,5 @@ cp -u config.rasi ~/.config/rofi/
 cp -u gruv.rasi ~/.config/rofi/
 mkdir ~/.config/alacritty
 cp -u alacritty.yml ~/.config/alacritty/
+
+echo "Use 'sudo nixos-rebuild switch' to complete the installation."
